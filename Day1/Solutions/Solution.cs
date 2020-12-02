fe96c23f41e6ba1d3c0ef0f5db56ab72318fedde
Chuck Fon Lee <ChuckFon.Lee@axxes.com>
2020-12-02 08:53:41 +0100

    optimized Day1

--- Day1/Solutions/Solution.cs
+++ Day1/Solutions/Solution.cs
@@ -1,47 +1,42 @@
 ﻿using AOC.Base;
 using System.Linq;
 
-namespace Day1.Solutions {
-    class Solution : Excercise<long> {
-        protected override long DoGold() {
+namespace Day1.Solutions
+{
+    class Solution : Excercise<long>
+    {
+        protected override long DoGold()
+        {
             var lines = ReadInput();
             var allNumbers = lines.Select(x => int.Parse(x));
-            var a = 0;
-            var b = 0;
-            var c = 0;
-            foreach (var number in allNumbers) {
-                if (a == 0 && b == 0 && c == 0) {
-                    foreach (var nu in allNumbers.Except(new int[] { number })) {
-                        var result = allNumbers.Except(new int[] { number, nu }).Where(y => y + number + nu == 2020).FirstOrDefault();
-                        if (result != default(int)) {
-                            a = number;
-                            b = result;
-                            c = nu;
-                        }
+            foreach (var number in allNumbers)
+            {
+                foreach (var nu in allNumbers.Except(new int[] { number }))
+                {
+                    var result = allNumbers.Except(new int[] { number, nu }).Where(y => y + number + nu == 2020).FirstOrDefault();
+                    if (result != default(int))
+                    {
+                        return result * number * nu;
                     }
                 }
             }
-
-            return a * b * c;
+            return 0;
         }
 
-        protected override long DoGrey() {
+        protected override long DoGrey()
+        {
             var lines = ReadInput();
             var allNumbers = lines.Select(x => int.Parse(x));
-            var a = 0;
-            var b = 0;
 
-            foreach (var number in allNumbers) {
-                if (a == 0 && b == 0) {
-                    var result = allNumbers.Except(new int[] { number }).Where(y => y + number == 2020).FirstOrDefault();
-                    if (result != default(int)) {
-                        a = number;
-                        b = result;
-                    }
+            foreach (var number in allNumbers)
+            {
+                var result = allNumbers.Except(new int[] { number }).Where(y => y + number == 2020).FirstOrDefault();
+                if (result != default(int))
+                {
+                    return result * number;
                 }
             }
-
-            return a * b;
+            return 0;
         }
     }
 }
